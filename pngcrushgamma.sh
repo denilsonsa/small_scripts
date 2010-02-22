b90414446e973c185261512b221e063a34bbad3c
Denilson Sá <denilsonsa@gmail.com>
2010-02-22 20:50:03 -0300

    Updated pngcrushgamma.sh help text and added -v option.

--- pngcrushgamma.sh
+++ pngcrushgamma.sh
@@ -4,28 +4,52 @@ MYNAME=`basename "$0"`
 
 print_help() {
 	cat << EOF
-Usage: $MYNAME file [ files... ]
+Usage: $MYNAME [-v] file [ files... ]
 
-This script will call pngcrush to remove gAMA chunk from PNG files, in order
-to make them work well inside Internet Explorer.
+This script will call pngcrush to remove gAMA chunk from PNG files, so that
+they will look fine inside Internet Explorer.
 
-In addition, other color-related chunks are removed as well.
+In addition, other color-related chunks are also removed: cHRM, iCCP, sRGB
+
+Option:
+  -v     verbose mode
+
+Read more:
+http://morris-photographics.com/photoshop/articles/png-gamma.html
+http://snook.ca/archives/design/which_image_for/#c21159
+http://www.hanselman.com/blog/GammaCorrectionAndColorCorrectionPNGIsStillTooHard.aspx
 EOF
 }
 
 
-if [  -z "$1"  -o  "$1" = "-h"  -o  "$1" = "--help"  ] ; then
+if [ -z "$1"  -o  "$1" = "-h"  -o  "$1" = "--help"  ] ; then
 	print_help
 	exit 1
 fi
 
+VERBOSE=0
+if [ "$1" = "-v" ] ; then
+	VERBOSE=1
+	shift
+fi
+
+
+if [ "$VERBOSE" = 1 ] ; then
+	PARAM=""
+else
+	PARAM="-q"
+fi
+
 for i in "$@" ; do
 	TMPFILE=`mktemp`
-	pngcrush -rem cHRM -rem gAMA -rem iCCP -rem sRGB "$i" "$TMPFILE"
+	pngcrush $PARAM -rem cHRM -rem gAMA -rem iCCP -rem sRGB "$i" "$TMPFILE"
 	# Damn pngcrush that does not correctly return an useful exit code
 	# -s FILE        True if file exists and is not empty.
 	if [ -s "$TMPFILE" ] ; then
+		echo "Updated file '$i'."
 		cat "$TMPFILE" > "$i"
+	else
+		echo "File '$i' was not changed."
 	fi
 	rm -f "$TMPFILE"
 done
