ed4aff2809fc6a44acdd91b5983e65bea6474555
Denilson Sá <denilsonsa@gmail.com>
2010-02-22 21:15:53 -0300

    Removed bash-ism from ls_video_length.sh
    Changed "&> /dev/null" to "> /dev/null 2> /dev/null". Now this script does not require bash (and works with dash).

--- ls_video_length.sh
+++ ls_video_length.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 for VID in "$@" ; do
 	LEN=`midentify "$VID" | sed -n 's/.*ID_LENGTH=\(.*\)/\1/p'`
-	if which printf &> /dev/null ; then
+	if which printf > /dev/null 2> /dev/null ; then
 		printf "%7s  %s\n" "$LEN" "$VID"
 	else
 		echo "$LEN  $VID"
