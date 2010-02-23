20522bebe2f5e54e398f847c0621eb8f3ff4faa0
Denilson Sá <denilsonsa@gmail.com>
2010-02-22 21:30:43 -0300

    Updated getHTTPFileSize.sh in 2008-02-02 to make it work even when the server redirects to another URL.

--- getHTTPFileSize.sh
+++ getHTTPFileSize.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #
 # Written by Denilson Figueiredo de Sa <denilsonsa@gmail.com>
+# 2008-02-02 - Fixed this script to work even when the server redirects to
+#              another URL. In this case, sometimes there are more than one
+#              Content-Length line, which is now handled properly.
 # 2008-01-20 - Removed bash dependency (now works also with dash).
 #              First time this script was published on my site.
 # 2004-02-29 - First version written.
@@ -26,7 +29,7 @@ EOF
 if [ "$#" != "1" -o "$1" = "-h" -o "$1" = "--help" ]; then
 	print_help
 else
-	LENGTH=`wget -S --spider "$1" 2>&1 | sed -n 's/^.*Content-Length: *\([0-9]\+\) *$/\1/p'`
+	LENGTH=`wget -S --spider "$1" 2>&1 | sed -n 's/^.*Content-Length: *\([0-9]\+\) *$/\1/p' | tail -n 1`
 	if [ "$LENGTH" != "" ]; then
 		echo "File size is: $(($LENGTH)) bytes, $(($LENGTH/1024)) KiB and $(($LENGTH/1024/1024)) MiB."
 	else
