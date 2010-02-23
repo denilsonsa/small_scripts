827314bb631e499cff0c6f32b11d9a99e8649736
Denilson Sá <denilsonsa@gmail.com>
2010-02-22 21:29:22 -0300

    Updated getHTTPFileSize.sh in 2008-01-20 to remove bash dependency. (now works with dash)

--- getHTTPFileSize.sh
+++ getHTTPFileSize.sh
@@ -1,44 +1,34 @@
-#!/bin/bash
-# getHTTPFileSize.sh - version 1.0 - 2004-02-29
-# Created by Denilson F. de Sa
+#!/bin/sh
+#
+# Written by Denilson Figueiredo de Sa <denilsonsa@gmail.com>
+# 2008-01-20 - Removed bash dependency (now works also with dash).
+#              First time this script was published on my site.
+# 2004-02-29 - First version written.
 
 # TODO
 # - Add support to show HTTP and wget errors
 
+MYNAME=`basename "$0"`
+
 print_help()
 {
-echo "getHTTPFileSize.sh - version 1.0 - 2004-02-29"
-echo "Usage: " `basename $0` " '<address>'"
-echo "Where <address> is the complete URL of the file."
-echo ""
-echo "This little script will retrieve headers from given URL and show the size of file, in bytes, kilobytes and megabytes (using division by 1024, and NOT 1000)."
+	cat << EOF
+Usage: $MYNAME <URL>
+
+This script will use wget to get the HTTP headers from the server and then
+parse these headers to print the file size (as reported by the server) in
+human-readable values.
+
+Only HTTP is supported.
+EOF
 }
 
-print_wget_notfound()
-{
-	echo "ERROR: wget was not found."
-	echo "This script uses wget, so you must have it somewhere in path."
-}
-
-print_sed_notfound()
-{
-	echo "ERROR: sed was not found."
-	echo "This script uses sed, so you must have it somewhere in path."
-}
-
-if [[ $# != 1 ]]; then
+if [ "$#" != "1" -o "$1" = "-h" -o "$1" = "--help" ]; then
 	print_help
 else
-	if ! which "wget" &>/dev/null; then
-		print_wget_notfound
-	fi
-	if ! which "sed" &>/dev/null; then
-		print_sed_notfound
-	fi
-
 	LENGTH=`wget -S --spider "$1" 2>&1 | sed -n 's/^.*Content-Length: *\([0-9]\+\) *$/\1/p'`
-	if [[ "$LENGTH" != "" ]]; then
-		echo "File size is: $(($LENGTH)) bytes, $(($LENGTH/1024)) KB and $(($LENGTH/1048576)) MB."
+	if [ "$LENGTH" != "" ]; then
+		echo "File size is: $(($LENGTH)) bytes, $(($LENGTH/1024)) KiB and $(($LENGTH/1024/1024)) MiB."
 	else
 		echo "File size is unknown."
 	fi
