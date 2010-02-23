5fbd8c64cc1f38d905dbf7ff39d5bfb39cbb6b17
Denilson Sá <denilsonsa@gmail.com>
2010-02-23 09:22:44 -0300

    Updated fat2ext.sh to the second version (sometime earlier than 2005-10-04)

--- fat2ext.sh
+++ fat2ext.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# fat2ext.sh - version 1.0 - 2003-11-16
+# fat2ext.sh - version 1.1 - 2003-11-16
 # Created by Denilson F. de Sa
 # 
 # This thing fix the permissions after you copy anything
@@ -12,53 +12,50 @@
 # This script calls itself recursively to go deeper inside
 # directory tree.
 # 
-# There are no option parameters. All parameters are
-# considered as directories.
-# 
 # This script is very verbose, it tells you ALL changes that
 # are made. Pipe it to a LOGFILE to see if everything is OK,
 # or to /dev/null if you dare! :) I'm not responsible if
 # anything goes wrong!
 
 
+# TODO / WishList
+# Create two more command-line options to ONLY change
+# files/directories that have <supplied> permissions.
+
+
 FILES=644
 DIRS=755
 
 
-if [ "$1" == "" ]; then
-	echo "Please use:
-  $0 .
-To run this on current directory."
+print_help()
+{
+echo "fat2ext.sh - version 1.1 - 2003-11-16"
+echo "Usage:" `basename $0` " <directory> [ file_permissions  dir_permissions ]"
+echo "Default permissions are 644 for files and 755 for directories"
+}
 
-fi
 
-PODEIR=0
+if [[ ($# != 3 && $# != 1) || "$1" == "-help" || "$1" == "--help" || "$1" == "-h" || "$1" == "-?" ]]; then
+	print_help
+else
 
-echo RUNNED AS "$*"
+	echo RUNNED AS "$*"
 
-while [ "$1" != "" ]; do
+	if [ "$3" != "" ]; then
+		FILES="$2"
+		DIRS="$3"
+	fi
+
+	#Changing workind dir
+	PODEIR=0
 	cd "$1" && PODEIR=1
-	pwd
 
+	#If changed, ok, go on!
 	if [ $PODEIR == 1 ]; then
+		pwd
 		chmod -v $FILES *
 		ls -1F --color=no | sed -n 's/\/$//p' | xargs -iAAA chmod -v $DIRS AAA
-		ls -1F --color=no | sed -n 's/\/$//p' | xargs -iAAA "$0" AAA
+		ls -1F --color=no | sed -n 's/\/$//p' | xargs -iAAA "$0" AAA "$FILES" "$DIRS"
 	fi
-	PODEIR=0
-	shift
-done
 
-
-
-
-
-# OLD VERSION
-#ls -1F --color=no | sed -n 's/\*$//p' | xargs -iAAA chmod -x -v AAA
-#Isto remove as permissões APENAS de arquivos. Em outras palavras, isto tira a permissão de executável de todos os executáveis.
-
-
-#ls -1F --color=no | sed -n 's/\/$//p' | xargs -iAAA find AAA | egrep '[^/]/[^/]' | xargs -iBBB chmod -v -x BBB
-#Isto remove as permissões de executável de todos os arquivos que estejam em apenas um nível abaixo do diretório atual.
-
-#Depois eu crio uma solução melhor, mais genérica, mais bem feita e mais funcional.
+fi
