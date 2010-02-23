e5e7c693db5d9d697ffa901fdbc9f950382ce477
Denilson Sá <denilsonsa@gmail.com>
2010-02-23 09:24:27 -0300

    Third version of fat2ext.sh, around 2005-10-04

--- fat2ext.sh
+++ fat2ext.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# fat2ext.sh - version 1.1 - 2003-11-16
-# Created by Denilson F. de Sa
+ABOUT='fat2ext.sh - version 1.2 - 2005-10-04
+Created by Denilson F. de Sa'
 # 
 # This thing fix the permissions after you copy anything
 # from a FAT partition to a ext2/ext3 one.
 # 
-# This version is recursive and verbose. It changes all
-# files to 644 and all directories to 755.
-# 
-# This script calls itself recursively to go deeper inside
-# directory tree.
-# 
-# This script is very verbose, it tells you ALL changes that
-# are made. Pipe it to a LOGFILE to see if everything is OK,
-# or to /dev/null if you dare! :) I'm not responsible if
-# anything goes wrong!
+# In fact, this script will change all directories permissions
+# to 755 and all files to 644. Any previous special permissions
+# are discarded.
+#
+# 755 and 644 are the default permissions. They can be changed
+# using command-line parameters.
 
 
 # TODO / WishList
@@ -25,11 +21,14 @@
 
 FILES=644
 DIRS=755
+VERBOSE=""
+#VERBOSE="-v"
 
 
 print_help()
 {
-echo "fat2ext.sh - version 1.1 - 2003-11-16"
+echo "$ABOUT"
+echo
 echo "Usage:" `basename $0` " <directory> [ file_permissions  dir_permissions ]"
 echo "Default permissions are 644 for files and 755 for directories"
 }
@@ -39,23 +38,23 @@ if [[ ($# != 3 && $# != 1) || "$1" == "-help" || "$1" == "--help" || "$1" == "-h
 	print_help
 else
 
-	echo RUNNED AS "$*"
-
 	if [ "$3" != "" ]; then
 		FILES="$2"
 		DIRS="$3"
 	fi
 
-	#Changing workind dir
-	PODEIR=0
-	cd "$1" && PODEIR=1
+	#This will give read permission to all files (and directories).
+	#After this command, 'find' will be able to access the entire directory tree.
+	chmod -R +rx "$1"
 
-	#If changed, ok, go on!
-	if [ $PODEIR == 1 ]; then
-		pwd
-		chmod -v $FILES *
-		ls -1F --color=no | sed -n 's/\/$//p' | xargs -iAAA chmod -v $DIRS AAA
-		ls -1F --color=no | sed -n 's/\/$//p' | xargs -iAAA "$0" AAA "$FILES" "$DIRS"
-	fi
+	find "$1" | ( while read line; do
+		if [ -d "$line" ] ;	then
+			chmod $VERBOSE "$DIRS" "$line"
+		elif [ -f "$line" ] ; then
+			chmod $VERBOSE "$FILES" "$line"
+		else
+			echo "Warning: '$line' is not a directory and is not a regular file."
+		fi
+	done )
 
 fi
