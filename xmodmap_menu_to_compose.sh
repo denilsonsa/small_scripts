c667cf9b2d830bf4896e97d995c5d4529298171c
Denilson Sá <denilsonsa@gmail.com>
2010-11-04 23:16:47 -0200

    Replaces lots of links with just a link to a post in my blog (that contains all the information in one place)

--- xmodmap_menu_to_compose.sh
+++ xmodmap_menu_to_compose.sh
@@ -8,15 +8,7 @@ xmodmap -e 'keycode 135 = Multi_key'
 # See the list of compose sequences:
 # /usr/share/X11/locale/en_US.UTF-8/Compose
 #
-# Wanna create your own sequences? Read this:
-# http://xorg.freedesktop.org/releases/X11R7.1/doc/RELNOTES4.html#30
-
-# References:
-# http://soft.zoneo.net/Linux/compose_key.php#xmodmap
+# See also: man Compose(5)
 #
-# http://wa5pb.freeshell.org/motd/?p=382
-# http://superuser.com/questions/154303/is-there-a-us-international-keyboard-layout-on-linux-that-mimics-windows-behavio
-#
-# https://help.ubuntu.com/community/ComposeKey
-# http://en.wikipedia.org/wiki/Compose_key
-# http://hermit.org/Linux/ComposeKeys.html
+# Detailed information on my blog:
+# http://my.opera.com/CrazyTerabyte/blog/2010/11/04/how-x11-xcompose-works
