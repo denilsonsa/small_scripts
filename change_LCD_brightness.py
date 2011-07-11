9848a254a51acd5e94ec3edec7849e34fcf6f487
Denilson Sá <denilsonsa@gmail.com>
2011-07-11 05:46:13 -0300

    Updated change_LCD_brightness.py to use /sys/class/backlight instead of /proc/acpi. The path is still hardcoded, and the script is still ugly. But it works. :)

--- change_LCD_brightness.py
+++ change_LCD_brightness.py
@@ -7,19 +7,31 @@ import os.path
 import subprocess
 import sys
 
-# Old, deprecated /proc/acpi path
-HARDCODED_PATH = "/proc/acpi/video/VGA/LCDD/brightness"
-# New, /sys path
-# However... This script must be rewritten to work with /sys/ interface
-# HARDCODED_PATH = "/sys/class/backlight/acpi_video0/brightness"
 
-HARCODED_SUDO_SCRIPT = "/usr/local/bin/set_lcd_brightness.sh"
+# Lots of things here are hard-coded...
+# This script is a bit ugly, but works.
+# And it evolved from a version that worked with /proc/acpi path (and that
+# explains a bit of the ugliness).
+
+
+# Old, deprecated /proc/acpi path
+#HARDCODED_PROC_PATH = "/proc/acpi/video/VGA/LCDD/brightness"
+
+# New, /sys path
+HARDCODED_SYS_PATH = "/sys/class/backlight/acpi_video0/"
+
+BRIGHTNESS_PATH = os.path.join(HARDCODED_SYS_PATH, "brightness")
+MAX_BRIGHTNESS_PATH = os.path.join(HARDCODED_SYS_PATH, "max_brightness")
+
+
+HARDCODED_SUDO_SCRIPT = "/usr/local/bin/set_lcd_brightness.sh"
 # The script contents:
 # #!/bin/sh
-# echo "$1" > /proc/acpi/video/VGA/LCDD/brightness
+# echo "$1" > /sys/class/backlight/acpi_video0/brightness
 #
 # The relevant /etc/sudoers line:
 # %users ALL = NOPASSWD: /usr/local/bin/set_lcd_brightness.sh
+
 
 def print_help():
     scriptname = os.path.basename(sys.argv[0])
@@ -30,23 +42,23 @@ def print_help():
         "  {name} 10   # Set brightness to exactly 10\n"
     ).format(name=scriptname)
 
-    levels, current = parse_proc_brightness()
+    levels, current = parse_sys_brightness()
     print "Current brightness: {0}".format(current)
     print "Available brightness levels:"
     print " ".join(levels)
 
-def parse_proc_brightness():
-    levels = []
-    current = None
-    with open(HARDCODED_PATH, "rb") as f:
-        for line in f:
-            key, sep, value = line.partition(":")
-            if key == "levels":
-                levels = value.split()
-            elif key == "current":
-                current = value.strip()
+
+def parse_sys_brightness():
+    with open(MAX_BRIGHTNESS_PATH, "rb") as f:
+        max_brightness = int(f.readline())
+
+    levels = [str(x) for x in range(max_brightness+1)]
+
+    with open(BRIGHTNESS_PATH, "rb") as f:
+        current = f.readline().strip()
 
     return levels, current
+
 
 def main():
     if len(sys.argv) != 2:
@@ -58,7 +70,7 @@ def main():
     relative = value_str[0] in "+-"
 
     if relative:
-        levels, current = parse_proc_brightness()
+        levels, current = parse_sys_brightness()
         index = levels.index(current)
         new_index = index + value
 
@@ -75,11 +87,11 @@ def main():
 
     try:
         # Can we write directly to that file?
-        with open(HARDCODED_PATH, "wb") as f:
+        with open(BRIGHTNESS_PATH, "wb") as f:
             f.write(new_value)
     except IOError:
         # So we can't write to it... But at least we have a sudo script.
-        subprocess.call(["sudo", "-n", HARCODED_SUDO_SCRIPT, new_value])
+        subprocess.call(["sudo", "-n", HARDCODED_SUDO_SCRIPT, new_value])
 
     # Optional xosd support
     # It works, but I didn't like it. Feel free to uncomment if you want.
