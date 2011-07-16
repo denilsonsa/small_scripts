28cc62a427627d23a7641a2d57844086173126a7
Denilson Sá <denilsonsa@gmail.com>
2011-07-16 15:07:24 -0300

    Adding comments on how to automatically configure the secondary keyboard with a different layout.

--- setxkbmap_secondary_keyboard.sh
+++ setxkbmap_secondary_keyboard.sh
@@ -18,5 +18,34 @@ echo_run() {
 KBD_ID=`xinput -list | sed -n 's/^.*Microsoft.*2\.4GHz Transceiver.*id=\([0-9]\+\).*slave \+keyboard.*$/\1/p'`
 
 if [ -n "$KBD_ID" ] ; then
-	echo_run setxkbmap -device "$KBD_ID" br abnt2 altwin:super_win numpad:windows
+	echo_run setxkbmap -device "$KBD_ID" br abnt2 numpad:microsoft
 fi
+
+
+# Configuring xorg.conf to automatically set the keyboard layout for this device:
+#
+# First, we need to find the Vendor and the Product from the device:
+# /sbin/udevadm info --query=all --name=/dev/input/by-id/usb-Microsoft_Microsoft®_2.4GHz_Transceiver_v8.0-event-kbd
+#
+# Source:
+#  https://bbs.archlinux.org/viewtopic.php?id=96422
+#
+#
+# Then we must add this code to xorg.conf (for xorg-server-1.8):
+#
+# Section "InputClass"
+#         Identifier "keyboard-microsoft-wireless"
+#         Driver "evdev"
+#         Option "XkbLayout" "br"
+#         Option "XkbVariant" "abnt2"
+#         Option "XkbOptions" "numpad:microsoft"
+#
+#         MatchIsKeyboard "on"
+#
+#         # The following rules apply substring matching:
+#         MatchVendor "Microsoft"
+#         MatchProduct "2.4GHz Transceiver"
+# EndSection
+#
+# Source:
+#  http://www.gentoo.org/proj/en/desktop/x/x11/xorg-server-1.8-upgrade-guide.xml
