ad10fa1689339a7c9330f7ef30cca75902cc8283
Denilson Sá <denilsonsa@gmail.com>
2012-02-14 22:27:41 -0200

    Adding a dead-simple Gravatar URL generator.

---
+++ gravatar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" -o "$1" = "-h" -o "$1" = "--help" ] ; then
+	echo 'Usage: gravatar.sh someone@example.com'
+	echo 'See also: http://en.gravatar.com/site/implement/images/'
+	exit 1
+fi
+
+MD5=`echo -n 'denilsonsa@gmail.com' | md5sum | sed 's/ \+- *$//'`
+echo "http://www.gravatar.com/avatar/${MD5}.jpg?s=512"
