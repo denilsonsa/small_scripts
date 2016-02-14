3643d53f1f9c2e429cf3e11c21a1c76ca4a2f23f
Denilson Sá <denilsonsa@gmail.com>
2016-02-13 22:20:58 -0200

    Enhancing the device auto-detection to include external displays.
    Now I can plug my external HDMI monitor and it will be auto-configured to my preferred layout. The code is smart enough to detect different display devices by serial number (through EDID).

--- auto_configure_upon_usb_device_change.py
+++ auto_configure_upon_usb_device_change.py
@@ -3,43 +3,79 @@
 # Based on:
 # http://askubuntu.com/questions/508236/how-can-i-run-code-whenever-a-usb-device-is-unplugged-without-requiring-root/
 # http://stackoverflow.com/questions/469243/how-can-i-listen-for-usb-device-inserted-events-in-linux-in-python
+# https://pyudev.readthedocs.org/en/latest/guide.html#asynchronous-monitoring
 
-import functools
 import os.path
 import pyudev
 import subprocess
-import time
+from threading import Timer
 
 
 def main():
     BASE_PATH = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
-    path = functools.partial(os.path.join, BASE_PATH)
+    path = lambda x: os.path.join(BASE_PATH, os.path.expanduser(x))
     call = lambda x, *args: subprocess.call([path(x)] + list(args))
 
-    context = pyudev.Context()
-    monitor = pyudev.Monitor.from_netlink(context)
-    monitor.filter_by(subsystem='usb')
-    monitor.start()
 
-    # Call them first:
-    call('synclient_asus_x450c.sh')
-    call('setxkbmap_my_preferences.sh')
+    timers = {
+        'usb': None,
+        'drm': None,
+    }
 
-    call('setxkbmap_secondary_keyboard.sh')
-    call('xinput_disable_mouse_acceleration.sh')
-    call('xset_my_preferences.sh')
-    call('xsetwacom_my_preferences.sh', 'desktop')
+    def reset_timer(which_one):
+        if timers[which_one] is not None:
+            timers[which_one].cancel()
+        timers[which_one] = None
 
-    # Call these again whenever a USB device is plugged or unplugged:
-    for device in iter(monitor.poll, None):
-        # Wait a short amount of time to let the device get ready.
-        time.sleep(0.250)
 
+    def usb_hotplug_callback():
         call('setxkbmap_secondary_keyboard.sh')
         call('xinput_disable_mouse_acceleration.sh')
         call('xset_my_preferences.sh')
         call('xsetwacom_my_preferences.sh', 'desktop')
 
+    def drm_hotplug_callback():
+        call('~/.screenlayout/z-auto-detect-displays.sh')
+
+    callbacks = {
+        'usb': usb_hotplug_callback,
+        'drm': drm_hotplug_callback,
+    }
+
+
+    def udev_event_received(device):
+        if device.subsystem in callbacks:
+            reset_timer(device.subsystem)
+            t = Timer(0.5, callbacks[device.subsystem])
+            timers[device.subsystem] = t
+            t.start()
+
+
+    # Call these first:
+    call('synclient_asus_x450c.sh')
+    call('setxkbmap_my_preferences.sh')
+    usb_hotplug_callback()
+
+    context = pyudev.Context()
+    # USB devices:
+    monitor_usb = pyudev.Monitor.from_netlink(context)
+    monitor_usb.filter_by(subsystem='usb')
+    observer_usb = pyudev.MonitorObserver(monitor_usb, callback=udev_event_received, daemon=False)
+    # External monitors (HDMI or VGA):
+    monitor_drm = pyudev.Monitor.from_netlink(context)
+    monitor_drm.filter_by(subsystem='drm')
+    observer_drm = pyudev.MonitorObserver(monitor_drm, callback=udev_event_received, daemon=False)
+
+    observer_usb.start()
+    observer_drm.start()
+
+    # This will prevent the program from finishing:
+    observer_usb.join()
+    observer_drm.join()
+
+    # Huh... Does not work? TODO: remove the daemon thing...
+    # The program will never exit because the observer threads are non-daemon.
+    # https://docs.python.org/3.5/library/threading.html#threading.Thread.daemon
 
 if __name__ == '__main__':
     main()
---
+++ screenlayout/z-auto-detect-displays.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# META:ICON = "display"
+#
+# Further reading:
+#
+# * https://askubuntu.com/questions/371261/display-monitor-info-via-command-line
+# * https://unix.stackexchange.com/questions/67983/get-monitor-make-and-model-and-other-info-in-human-readable-form
+# * https://stackoverflow.com/questions/10500521/linux-retrieve-monitor-names
+# * https://github.com/MestreLion/scripts/blob/master/monitor-switch
+# * https://en.wikipedia.org/wiki/Extended_Display_Identification_Data
+# * apt-get install read-edid:
+#     * http://polypux.org/projects/read-edid/
+# * apt-get install edid-decode:
+#     * https://cgit.freedesktop.org/xorg/app/edid-decode
+# * /sys/class/drm/card*-*/edid
+# * xrandr --prop
+
+
+# Input: xrandr --prop
+# Output:
+# LVDS1 connected 00ffffffffffff004ca349360000000000150103801f11780a09e59757548a27225054000000010101010101010101010101010101017d1e5618510016303020250035ae100000190000000f0000000000000000001ec8066a00000000fe0053414d53554e470a2020202020000000fe004c544e313430415432305030310021
+# DP1 disconnected
+# HDMI1 connected 00ffffffffffff0005e35123b00500002317010380331d782a77c5a3544f9f27115054bfef00d1c0b30095008180814081c001010101023a801871382d40582c4500fd1e1100001e000000fd00324c1e5311000a202020202020000000fc00323335310a2020202020202020000000ff0032333033384941303031343536016b02031ef14b0514101f04130312021101230907018301000065030c0010008c0ad08a20e02d10103e9600fd1e11000018011d007251d01e206e285500fd1e1100001e8c0ad08a20e02d10103e9600fd1e110000188c0ad090204031200c405500fd1e110000180000000000000000000000000000000000000000000000000083
+# VGA1 disconnected
+extract_edids_from_xrandr_input() {
+	sed -n '
+		# Printing and clearing the hold space.
+		/^\([^ \t]\|$\)/ {
+			x
+			/.\+/ {
+				s/\n\+//g
+				s/ \+$//
+				p
+			}
+			s/.*//
+			x
+		}
+
+		# Start of each display/connection.
+		/^[^ \t].*\(dis\)\?connected/ {
+			s/^\([^ \t]\+\) \+\(\(dis\)\?connected\) .*/\1 \2 /
+			h
+		}
+
+		# EDID section.
+		/^[ \t]\+EDID:/,/\(:\|[^ \t][ \t]\+[^ \t]\)/ {
+			/^[ \t]\+[0-9a-fA-F]\+[ \t]*$/ {
+				s/[ \t]\+//g
+				H
+			}
+		}
+	'
+}
+
+
+# Input: the output of extract_edids_from_xrandr_input
+# Output:
+# LVDS1 connected 4ca34936000000000015
+# DP1 disconnected
+# HDMI1 connected 05e35123b00500002317
+# VGA1 disconnected
+replace_edid_with_serial() {
+	# Extracting the serial number from the full EDID.
+	sed 's/ 00FFFFFFFFFFFF00\([0-9a-fA-F]\{20\}\)[0-9a-fA-F]\+/ \1/i'
+}
+
+# Input: the output of replace_edid_with_serial
+# Output (sorted and upper-case):
+# LVDS1 4CA34936000000000015
+# HDMI1 05E35123B00500002317
+extract_only_connected() {
+	tr '[a-z]' '[A-Z]' | sed -n '
+		s/^\([^ ]\+\) CONNECTED \([0-9A-Z]\+\)$/\1 \2/p
+	'
+}
+
+
+export LC_ALL=C
+
+status="$(
+xrandr --prop \
+	| extract_edids_from_xrandr_input \
+	| replace_edid_with_serial \
+	| extract_only_connected
+)"
+
+nl=$'\n'
+laptop='4CA34936000000000015'
+aoc2351='05E35123B00500002317'
+
+case "${status}" in
+	"LVDS1 ${laptop}")
+		~/.screenlayout/video-only-laptop.sh
+		;;
+	"LVDS1 ${laptop}${nl}HDMI1 ${aoc2351}")
+		~/.screenlayout/video-1080p-at-right.sh
+		;;
+	*)
+		echo 'Unknown configuration:'
+		echo "${status}"
+		;;
+esac
