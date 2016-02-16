44920a4cb98aaf0a9921df001a5baef4592e8232
Denilson Sá <denilsonsa@gmail.com>
2016-02-16 07:33:30 -0200

    Fixing random crashes in older pyudev and older Python.

--- auto_configure_upon_usb_device_change.py
+++ auto_configure_upon_usb_device_change.py
@@ -1,9 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 #
 # Based on:
 # http://askubuntu.com/questions/508236/how-can-i-run-code-whenever-a-usb-device-is-unplugged-without-requiring-root/
 # http://stackoverflow.com/questions/469243/how-can-i-listen-for-usb-device-inserted-events-in-linux-in-python
 # https://pyudev.readthedocs.org/en/latest/guide.html#asynchronous-monitoring
+#
+#
+# Troubleshooting:
+#
+# Problem: The script aborts after waking the laptop from sleep with the following error:
+#
+# Exception in thread Thread-2:
+# Traceback (most recent call last):
+#   File "/usr/lib/python3.4/threading.py", line 920, in _bootstrap_inner
+#     self.run()
+#   File "/usr/lib/python3/dist-packages/pyudev/monitor.py", line 506, in run
+#     for fd, _ in notifier.poll():
+# InterruptedError: [Errno 4] Interrupted system call
+#
+# Exception in thread Thread-1:
+# Traceback (most recent call last):
+#   File "/usr/lib/python3.4/threading.py", line 920, in _bootstrap_inner
+#     self.run()
+#   File "/usr/lib/python3/dist-packages/pyudev/monitor.py", line 506, in run
+#     for fd, _ in notifier.poll():
+# InterruptedError: [Errno 4] Interrupted system call
+#
+# Cause/solutions:
+#
+# * Update pyudev to version 0.17 (Aug 26, 2015) or later.
+#     * https://github.com/pyudev/pyudev/commit/9dbcbf598f5eb77d8972e8d4e368e2fd1afecda8
+#     * Ubuntu 15.10 (wily) still has version 0.16.
+#     * Ubuntu xenial doesn't seem to update the version, yet.
+#     * http://packages.ubuntu.com/search?keywords=python-pyudev
+# * Update Python to 3.5 or later.
+#     * https://docs.python.org/3/whatsnew/3.5.html#pep-475-retry-system-calls-failing-with-eintr
+#     * Ubuntu 15.10 (wily) has Python 3.5, although it is not the default interpreter.
+
 
 import os.path
 import pyudev
