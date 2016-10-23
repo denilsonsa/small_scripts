6d099f3034e2c0756c83653eae5037bb7375dd93
Denilson Sá Maia <denilsonsa@gmail.com>
2016-10-24 01:37:38 +0200

    Adding more screen layouts.

---
+++ screenlayout/video-1080i-at-top.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# META:ICON = "video-1080p-at-top.png"
+xrandr --dpi 96x96 --output HDMI-1 --mode 1920x1080i --pos 0x0 --rotate normal --output LVDS-1 --mode 1366x768 --pos 554x1080 --rotate normal --primary --output VIRTUAL-1 --off --output DP-1 --off --output VGA-1 --off
+~/myrepos/small_scripts/wallpaper_restore.sh
+~/.screenlayout/audio-to-HDMI.sh
+~/.screenlayout/wacom-desktop.sh
+
+# --dpi 96x96 because: https://www.reddit.com/r/chrome/comments/36u42i/psa_set_dpi_to_96x96_to_fix_several_chrome_43/
