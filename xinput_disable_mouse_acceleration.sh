892f75f79d3c51a7145236c7bdf0047a3c9bf704
Denilson Sá Maia <denilsonsa@gmail.com>
2018-07-30 15:42:43 +0200

    More mice to this script

--- xinput_disable_mouse_acceleration.sh
+++ xinput_disable_mouse_acceleration.sh
@@ -11,6 +11,12 @@ xsp_microsoft() {
 xsp_logitech() {
 	echo_run xinput set-prop 'Logitech USB Laser Mouse' "$@"
 }
+xsp_logitech2() {
+	echo_run xinput set-prop 'Logitech M705' "$@"
+}
+xsp_dell() {
+	echo_run xinput set-prop 'PixArt Dell MS116 USB Optical Mouse' "$@"
+}
 
 # Device Accel Profile (259):	0
 # Device Accel Constant Deceleration (260):	1.000000
@@ -23,6 +29,7 @@ xsp_logitech() {
 # http://xorg.freedesktop.org/wiki/Development/Documentation/PointerAcceleration/#accelerationprofileinteger
 xsp_microsoft 'Device Accel Profile' -1
 xsp_logitech 'Device Accel Profile' -1
+xsp_logitech2 'Device Accel Profile' -1
 
 # Constant mouse speed:
 xsp_microsoft 'Device Accel Constant Deceleration' 1.5
