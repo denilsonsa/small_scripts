5628288b4564654fc7e314b1deb54a5bfe424f85
Denilson Sá Maia <denilsonsa@gmail.com>
2018-12-23 05:25:36 +0100

    Comments and PEP-8 changes
    But I insist to ignore PEP-8 recommendation on line length.

--- xinput_configure_all_my_devices.py
+++ xinput_configure_all_my_devices.py
@@ -53,7 +53,6 @@ NATURAL_SCROLLING = [
 ]
 
 
-
 TOUCHPAD_COMMON_SETTINGS = [
     # Vertical and horizontal scrolling.
     ['set-prop', 'Synaptics Two-Finger Scrolling', 1, 1],
@@ -194,6 +193,10 @@ rules_list = [
                 'apple:alupckeys',
                 'lv3:rwin_switch',
             ],
+
+            # fn is in place of the usual Insert key.
+            # Thus, remapping Eject to Insert
+            # xmodmap -e 'keysym XF86Eject = Insert NoSymbol Insert'
         ]
     ),
 
@@ -204,42 +207,41 @@ rules_list = [
             # Asus X450C laptop.
             'ETPS/2 Elantech Touchpad',
 
-# (II) config/udev: Adding input device ETPS/2 Elantech Touchpad (/dev/input/event9)
-# (**) ETPS/2 Elantech Touchpad: Applying InputClass "evdev touchpad catchall"
-# (**) ETPS/2 Elantech Touchpad: Applying InputClass "touchpad catchall"
-# (**) ETPS/2 Elantech Touchpad: Applying InputClass "Default clickpad buttons"
-# (II) LoadModule: "synaptics"
-# (II) Loading /usr/lib/xorg/modules/input/synaptics_drv.so
-# (II) Module synaptics: vendor="X.Org Foundation"
-# 	compiled for 1.14.2, module version = 1.7.1
-# 	Module class: X.Org XInput Driver
-# 	ABI class: X.Org XInput driver, version 19.1
-# (II) Using input driver 'synaptics' for 'ETPS/2 Elantech Touchpad'
-# (**) ETPS/2 Elantech Touchpad: always reports core events
-# (**) Option "Device" "/dev/input/event9"
-# (II) synaptics: ETPS/2 Elantech Touchpad: found clickpad property
-# (--) synaptics: ETPS/2 Elantech Touchpad: x-axis range 0 - 3260 (res 32)
-# (--) synaptics: ETPS/2 Elantech Touchpad: y-axis range 0 - 1793 (res 32)
-# (--) synaptics: ETPS/2 Elantech Touchpad: pressure range 0 - 255
-# (--) synaptics: ETPS/2 Elantech Touchpad: finger width range 0 - 15
-# (--) synaptics: ETPS/2 Elantech Touchpad: buttons: left double triple
-# (--) synaptics: ETPS/2 Elantech Touchpad: Vendor 0x2 Product 0xe
-# (**) Option "SoftButtonAreas" "50% 0 82% 0 0 0 0 0"
-# (--) synaptics: ETPS/2 Elantech Touchpad: touchpad found
-# (**) ETPS/2 Elantech Touchpad: always reports core events
-# (**) Option "config_info" "udev:/sys/devices/platform/i8042/serio4/input/input9/event9"
-# (II) XINPUT: Adding extended input device "ETPS/2 Elantech Touchpad" (type: TOUCHPAD, id 12)
-# (**) synaptics: ETPS/2 Elantech Touchpad: (accel) MinSpeed is now constant deceleration 2.5
-# (**) synaptics: ETPS/2 Elantech Touchpad: (accel) MaxSpeed is now 1.75
-# (**) synaptics: ETPS/2 Elantech Touchpad: (accel) AccelFactor is now 0.054
-# (**) ETPS/2 Elantech Touchpad: (accel) keeping acceleration scheme 1
-# (**) ETPS/2 Elantech Touchpad: (accel) acceleration profile 1
-# (**) ETPS/2 Elantech Touchpad: (accel) acceleration factor: 2.000
-# (**) ETPS/2 Elantech Touchpad: (accel) acceleration threshold: 4
-# (--) synaptics: ETPS/2 Elantech Touchpad: touchpad found
-# (II) config/udev: Adding input device ETPS/2 Elantech Touchpad (/dev/input/mouse0)
-# (**) ETPS/2 Elantech Touchpad: Ignoring device from InputClass "touchpad ignore duplicates"
-
+            # (II) config/udev: Adding input device ETPS/2 Elantech Touchpad (/dev/input/event9)
+            # (**) ETPS/2 Elantech Touchpad: Applying InputClass "evdev touchpad catchall"
+            # (**) ETPS/2 Elantech Touchpad: Applying InputClass "touchpad catchall"
+            # (**) ETPS/2 Elantech Touchpad: Applying InputClass "Default clickpad buttons"
+            # (II) LoadModule: "synaptics"
+            # (II) Loading /usr/lib/xorg/modules/input/synaptics_drv.so
+            # (II) Module synaptics: vendor="X.Org Foundation"
+            # 	compiled for 1.14.2, module version = 1.7.1
+            # 	Module class: X.Org XInput Driver
+            # 	ABI class: X.Org XInput driver, version 19.1
+            # (II) Using input driver 'synaptics' for 'ETPS/2 Elantech Touchpad'
+            # (**) ETPS/2 Elantech Touchpad: always reports core events
+            # (**) Option "Device" "/dev/input/event9"
+            # (II) synaptics: ETPS/2 Elantech Touchpad: found clickpad property
+            # (--) synaptics: ETPS/2 Elantech Touchpad: x-axis range 0 - 3260 (res 32)
+            # (--) synaptics: ETPS/2 Elantech Touchpad: y-axis range 0 - 1793 (res 32)
+            # (--) synaptics: ETPS/2 Elantech Touchpad: pressure range 0 - 255
+            # (--) synaptics: ETPS/2 Elantech Touchpad: finger width range 0 - 15
+            # (--) synaptics: ETPS/2 Elantech Touchpad: buttons: left double triple
+            # (--) synaptics: ETPS/2 Elantech Touchpad: Vendor 0x2 Product 0xe
+            # (**) Option "SoftButtonAreas" "50% 0 82% 0 0 0 0 0"
+            # (--) synaptics: ETPS/2 Elantech Touchpad: touchpad found
+            # (**) ETPS/2 Elantech Touchpad: always reports core events
+            # (**) Option "config_info" "udev:/sys/devices/platform/i8042/serio4/input/input9/event9"
+            # (II) XINPUT: Adding extended input device "ETPS/2 Elantech Touchpad" (type: TOUCHPAD, id 12)
+            # (**) synaptics: ETPS/2 Elantech Touchpad: (accel) MinSpeed is now constant deceleration 2.5
+            # (**) synaptics: ETPS/2 Elantech Touchpad: (accel) MaxSpeed is now 1.75
+            # (**) synaptics: ETPS/2 Elantech Touchpad: (accel) AccelFactor is now 0.054
+            # (**) ETPS/2 Elantech Touchpad: (accel) keeping acceleration scheme 1
+            # (**) ETPS/2 Elantech Touchpad: (accel) acceleration profile 1
+            # (**) ETPS/2 Elantech Touchpad: (accel) acceleration factor: 2.000
+            # (**) ETPS/2 Elantech Touchpad: (accel) acceleration threshold: 4
+            # (--) synaptics: ETPS/2 Elantech Touchpad: touchpad found
+            # (II) config/udev: Adding input device ETPS/2 Elantech Touchpad (/dev/input/mouse0)
+            # (**) ETPS/2 Elantech Touchpad: Ignoring device from InputClass "touchpad ignore duplicates"
         ],
         [
             *TOUCHPAD_COMMON_SETTINGS,
@@ -273,61 +275,60 @@ rules_list = [
             'AlpsPS/2 ALPS DualPoint TouchPad',
             'AlpsPS/2 ALPS GlidePoint',
 
-# (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint TouchPad (/dev/input/event7)
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "evdev touchpad catchall"
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "libinput touchpad catchall"
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "touchpad catchall"
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "Default clickpad buttons"
-# (II) LoadModule: "synaptics"
-# (II) Loading /usr/lib/xorg/modules/input/synaptics_drv.so
-# (II) Module synaptics: vendor="X.Org Foundation"
-# 	compiled for 1.19.3, module version = 1.9.0
-# 	Module class: X.Org XInput Driver
-# 	ABI class: X.Org XInput driver, version 24.1
-# (II) Using input driver 'synaptics' for 'AlpsPS/2 ALPS DualPoint TouchPad'
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: always reports core events
-# (**) Option "Device" "/dev/input/event7"
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: x-axis range 0 - 2432 (res 34)
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: y-axis range 0 - 1280 (res 34)
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: pressure range 0 - 127
-# (II) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: device does not report finger width.
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: buttons: left right middle double triple
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: Vendor 0x2 Product 0x8
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: invalid finger width range.  defaulting to 0 - 15
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: touchpad found
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: always reports core events
-# (**) Option "config_info" "udev:/sys/devices/platform/i8042/serio1/input/input6/event7"
-# (II) XINPUT: Adding extended input device "AlpsPS/2 ALPS DualPoint TouchPad" (type: TOUCHPAD, id 14)
-# (**) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: (accel) MinSpeed is now constant deceleration 2.5
-# (**) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: (accel) MaxSpeed is now 1.75
-# (**) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: (accel) AccelFactor is now 0.073
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) keeping acceleration scheme 1
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) acceleration profile 1
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) acceleration factor: 2.000
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) acceleration threshold: 4
-# (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: touchpad found
-# (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint TouchPad (/dev/input/mouse1)
-# (**) AlpsPS/2 ALPS DualPoint TouchPad: Ignoring device from InputClass "touchpad ignore duplicates"
-# (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint Stick (/dev/input/event6)
-# (**) AlpsPS/2 ALPS DualPoint Stick: Applying InputClass "evdev pointer catchall"
-# (**) AlpsPS/2 ALPS DualPoint Stick: Applying InputClass "trackpoint catchall"
-# (**) AlpsPS/2 ALPS DualPoint Stick: Applying InputClass "libinput pointer catchall"
-# (II) Using input driver 'libinput' for 'AlpsPS/2 ALPS DualPoint Stick'
-# (**) AlpsPS/2 ALPS DualPoint Stick: always reports core events
-# (**) Option "Device" "/dev/input/event6"
-# (**) Option "_source" "server/udev"
-# (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is tagged by udev as: Mouse Pointingstick
-# (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is a pointer caps
-# (**) Option "config_info" "udev:/sys/devices/platform/i8042/serio1/input/input8/event6"
-# (II) XINPUT: Adding extended input device "AlpsPS/2 ALPS DualPoint Stick" (type: MOUSE, id 15)
-# (**) Option "AccelerationScheme" "none"
-# (**) AlpsPS/2 ALPS DualPoint Stick: (accel) selected scheme none/0
-# (**) AlpsPS/2 ALPS DualPoint Stick: (accel) acceleration factor: 2.000
-# (**) AlpsPS/2 ALPS DualPoint Stick: (accel) acceleration threshold: 4
-# (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is tagged by udev as: Mouse Pointingstick
-# (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is a pointer caps
-# (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint Stick (/dev/input/mouse0)
-
+            # (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint TouchPad (/dev/input/event7)
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "evdev touchpad catchall"
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "libinput touchpad catchall"
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "touchpad catchall"
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: Applying InputClass "Default clickpad buttons"
+            # (II) LoadModule: "synaptics"
+            # (II) Loading /usr/lib/xorg/modules/input/synaptics_drv.so
+            # (II) Module synaptics: vendor="X.Org Foundation"
+            # 	compiled for 1.19.3, module version = 1.9.0
+            # 	Module class: X.Org XInput Driver
+            # 	ABI class: X.Org XInput driver, version 24.1
+            # (II) Using input driver 'synaptics' for 'AlpsPS/2 ALPS DualPoint TouchPad'
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: always reports core events
+            # (**) Option "Device" "/dev/input/event7"
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: x-axis range 0 - 2432 (res 34)
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: y-axis range 0 - 1280 (res 34)
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: pressure range 0 - 127
+            # (II) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: device does not report finger width.
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: buttons: left right middle double triple
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: Vendor 0x2 Product 0x8
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: invalid finger width range.  defaulting to 0 - 15
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: touchpad found
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: always reports core events
+            # (**) Option "config_info" "udev:/sys/devices/platform/i8042/serio1/input/input6/event7"
+            # (II) XINPUT: Adding extended input device "AlpsPS/2 ALPS DualPoint TouchPad" (type: TOUCHPAD, id 14)
+            # (**) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: (accel) MinSpeed is now constant deceleration 2.5
+            # (**) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: (accel) MaxSpeed is now 1.75
+            # (**) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: (accel) AccelFactor is now 0.073
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) keeping acceleration scheme 1
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) acceleration profile 1
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) acceleration factor: 2.000
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: (accel) acceleration threshold: 4
+            # (--) synaptics: AlpsPS/2 ALPS DualPoint TouchPad: touchpad found
+            # (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint TouchPad (/dev/input/mouse1)
+            # (**) AlpsPS/2 ALPS DualPoint TouchPad: Ignoring device from InputClass "touchpad ignore duplicates"
+            # (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint Stick (/dev/input/event6)
+            # (**) AlpsPS/2 ALPS DualPoint Stick: Applying InputClass "evdev pointer catchall"
+            # (**) AlpsPS/2 ALPS DualPoint Stick: Applying InputClass "trackpoint catchall"
+            # (**) AlpsPS/2 ALPS DualPoint Stick: Applying InputClass "libinput pointer catchall"
+            # (II) Using input driver 'libinput' for 'AlpsPS/2 ALPS DualPoint Stick'
+            # (**) AlpsPS/2 ALPS DualPoint Stick: always reports core events
+            # (**) Option "Device" "/dev/input/event6"
+            # (**) Option "_source" "server/udev"
+            # (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is tagged by udev as: Mouse Pointingstick
+            # (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is a pointer caps
+            # (**) Option "config_info" "udev:/sys/devices/platform/i8042/serio1/input/input8/event6"
+            # (II) XINPUT: Adding extended input device "AlpsPS/2 ALPS DualPoint Stick" (type: MOUSE, id 15)
+            # (**) Option "AccelerationScheme" "none"
+            # (**) AlpsPS/2 ALPS DualPoint Stick: (accel) selected scheme none/0
+            # (**) AlpsPS/2 ALPS DualPoint Stick: (accel) acceleration factor: 2.000
+            # (**) AlpsPS/2 ALPS DualPoint Stick: (accel) acceleration threshold: 4
+            # (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is tagged by udev as: Mouse Pointingstick
+            # (II) input device 'AlpsPS/2 ALPS DualPoint Stick', /dev/input/event6 is a pointer caps
+            # (II) config/udev: Adding input device AlpsPS/2 ALPS DualPoint Stick (/dev/input/mouse0)
         ],
         [
             *TOUCHPAD_COMMON_SETTINGS,
@@ -424,6 +425,7 @@ def xinput_list():
             match = xinput_ignored_regex.match(line)
             if not match:
                 print('Warning! Unrecognized line from `xinput list` output: {0!r}'.format(line))
+
 
 def build_cmdline_args(device, action):
     '''Builds the command-line to be executed.
