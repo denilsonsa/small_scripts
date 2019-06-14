160da3aa4bb36eb0231e249b3a09b65cd83362b1
Denilson Sá Maia <denilsonsa@gmail.com>
2019-06-14 10:48:11 +0200

    Adding restart_usb_subsystem.sh
    Copied from http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux/

---
+++ restart_usb_subsystem.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# This cript disables and then re-enables the USB drivers.
+#
+# Copied from http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux/
+
+if [[ $EUID != 0 ]] ; then
+  echo This must be run as root!
+  exit 1
+fi
+
+for xhci in /sys/bus/pci/drivers/?hci_hcd ; do
+
+  if ! cd $xhci ; then
+    echo Weird error. Failed to change directory to $xhci
+    exit 1
+  fi
+
+  echo Resetting devices from $xhci...
+
+  for i in ????:??:??.? ; do
+    echo -n "$i" > unbind
+    echo -n "$i" > bind
+  done
+done
