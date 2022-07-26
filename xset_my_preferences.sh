d31a1705832ec9ed9b3aa3a1bee8119e0a963562
Denilson Sá Maia <denilsonsa@gmail.com>
2022-07-26 13:21:54 +0200

    Not setting mouse acceleration through xset.
    I've heard xset is kind of deprecated, but I see no other way to set the
    keyboard repeat rate.
    
    Well, at least for mouse acceleration it's bad, as xinput allows me to
    configure it individually per device.

--- xset_my_preferences.sh
+++ xset_my_preferences.sh
@@ -2,7 +2,8 @@
 
 # Keyboard key repeat rate.
 #xset r rate 500 20
-xset r rate 400 25
+xset r rate 350 25
 
 # Disabling mouse acceleration.
-xset m 1/1
+# Well, not anymore. I'm doing it through xinput.
+#xset m 1/1
