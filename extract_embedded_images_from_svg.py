373eb8621b6d55274b60eb706bab57f90a5cfbc3
Denilson Sá Maia <denilsonsa@gmail.com>
2022-12-02 10:40:21 +0100

    Style changes to extract_embedded_images_from_svg.py
    Ran the code through `black` tool to get to a uniform style.

--- extract_embedded_images_from_svg.py
+++ extract_embedded_images_from_svg.py
@@ -24,25 +24,30 @@ import xml.etree.ElementTree
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='Extracts all embedded images inside an SVG file.',
-        epilog=textwrap.dedent('''
-        SVGZ (compressed SVG) files are not supported directly, uncompress them first:
-        gunzip -c foobar.svgz | {0} -
+        description="Extracts all embedded images inside an SVG file.",
+        epilog=textwrap.dedent(
+            """
+            SVGZ (compressed SVG) files are not supported directly, uncompress them first:
+            gunzip -c foobar.svgz | {0} -
 
-        For extracting images from PDF files, try 'pdfimages -j' from poppler package.
-        '''.format(os.path.basename(sys.argv[0]))),
-        formatter_class=argparse.RawDescriptionHelpFormatter
+            For extracting images from PDF files, try 'pdfimages -j' from poppler package.
+            """.format(
+                os.path.basename(sys.argv[0])
+            )
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
-        '-p', '--prefix',
-        action='store',
-        help='prefix when saving the extracted images (default: the SVG pathname)'
+        "-p",
+        "--prefix",
+        action="store",
+        help="prefix when saving the extracted images (default: the SVG pathname)",
     )
     parser.add_argument(
-        'svgfile',
-        action='store',
-        type=argparse.FileType('rb'),
-        help='the SVG file containing embedded images'
+        "svgfile",
+        action="store",
+        type=argparse.FileType("rb"),
+        help="the SVG file containing embedded images",
     )
     args = parser.parse_args()
 
@@ -53,9 +58,9 @@ def decode_data_url(url):
     # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs
     # Default media type for data URLs.
     # It's nonsensical in the context of this script, though.
-    mediatype = 'text/plain'
+    mediatype = "text/plain"
 
-    match = re.match(r'^data:([^,;]+)', url)
+    match = re.match(r"^data:([^,;]+)", url)
     if match:
         mediatype = match.group(1)
 
@@ -65,25 +70,25 @@ def decode_data_url(url):
 # Based on:
 # https://github.com/django/django/blob/e2ae8b048e7198428f696375b8bdcd89e90002d1/django/utils/text.py#L213
 def get_valid_filename(s):
-    s = s.strip().replace(' ', '_')
-    return re.sub(r'(?u)[^-\w.]', '', s)
+    s = s.strip().replace(" ", "_")
+    return re.sub(r"(?u)[^-\w.]", "", s)
 
 
 def stripNS(s):
-    '''Receives either a tag name or an attribute name, returns the name
+    """Receives either a tag name or an attribute name, returns the name
     without the namespace.
 
     >>> stripNS('{http://www.w3.org/2000/svg}image')
     'image'
     >>> stripNS('id')
     'id'
-    '''
-    return s.rpartition('}')[2]
+    """
+    return s.rpartition("}")[2]
 
 
 def extract_from_svg(svgfile, prefix):
     # Creating dirs for prefix.
-    parts = prefix.rpartition('/')
+    parts = prefix.rpartition("/")
     prefix_dir = parts[0] + parts[1]
     prefix_file = parts[2]
     if prefix_dir:
@@ -95,44 +100,44 @@ def extract_from_svg(svgfile, prefix):
         # https://developer.mozilla.org/en-US/docs/Web/SVG/Element/image
         # “The only image formats SVG software must support are JPEG, PNG, and
         # other SVG files. Animated GIF behavior is undefined.”
-        'image/gif': '.gif',
-        'image/jpeg': '.jpg',
-        'image/png': '.png',
-        'image/svg+xml': '.svg',  # Inception!?
+        "image/gif": ".gif",
+        "image/jpeg": ".jpg",
+        "image/png": ".png",
+        "image/svg+xml": ".svg",  # Inception!?
         # To make this script future-proof, I'm adding a few extra image types.
         # These do not happen in the wild yet (or ever), and are likely
         # unsupported by most tools.
-        'image/avif': '.avif',
-        'image/bmp': '.bmp',
-        'image/tiff': '.tif',
-        'image/vnd.microsoft.icon': '.ico',
-        'image/webp': '.webp',
+        "image/avif": ".avif",
+        "image/bmp": ".bmp",
+        "image/tiff": ".tif",
+        "image/vnd.microsoft.icon": ".ico",
+        "image/webp": ".webp",
     }
 
-    for (event, elem) in xml.etree.ElementTree.iterparse(svgfile, ['start']):
+    for (event, elem) in xml.etree.ElementTree.iterparse(svgfile, ["start"]):
         tag = stripNS(elem.tag)
-        if tag == 'image':
+        if tag == "image":
             attrs = {stripNS(k): v for k, v in elem.attrib.items()}
-            if 'href' in attrs:
-                url = attrs['href']
-                if url.startswith('data:'):
+            if "href" in attrs:
+                url = attrs["href"]
+                if url.startswith("data:"):
                     data, mimetype = decode_data_url(url)
                     image_count += 1
-                    sane_id = get_valid_filename(attrs.get('id', ''))
-                    extension = mime_to_extension.get(mimetype, '')
-                    out_filename = '{0}{1}{2}{3}'.format(
-                            prefix,
-                            image_count,
-                            '-' + sane_id if sane_id else '',
-                            extension)
-                    with open(out_filename, 'wb') as out:
+                    sane_id = get_valid_filename(attrs.get("id", ""))
+                    extension = mime_to_extension.get(mimetype, "")
+                    out_filename = "{0}{1}{2}{3}".format(
+                        prefix, image_count, "-" + sane_id if sane_id else "", extension
+                    )
+                    with open(out_filename, "wb") as out:
                         out.write(data)
 
 
 def main():
     options = parse_arguments()
     if options.prefix is None:
-        options.prefix = options.svgfile.name if options.svgfile.name != '<stdin>' else 'stdin'
+        options.prefix = (
+            options.svgfile.name if options.svgfile.name != "<stdin>" else "stdin"
+        )
 
     extract_from_svg(options.svgfile, options.prefix)
 
