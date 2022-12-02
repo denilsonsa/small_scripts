d55e5992e70dd54bde4d6c238a7a1619716cc04f
Denilson Sá Maia <denilsonsa@gmail.com>
2022-12-02 12:30:12 +0100

    watch_website_tkinter.py is now ported to Python 3
    It would be nice to provide parameters using argparse, or even a better
    GUI. But this was an one-off script that I used once or twice.

--- watch_website_tkinter.py
+++ watch_website_tkinter.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 # vi:ts=4 sw=4 et
 
 # Quick and dirty script to keep refreshing a page and telling the user
@@ -10,9 +9,9 @@
 # https://gist.github.com/740278
 
 import datetime
-import urllib2
-
-import Tkinter
+import urllib.error
+import urllib.request
+import tkinter
 
 
 SITE_URL = "http://equipe.nce.ufrj.br/eber/FES20102/default2.htm"
@@ -23,34 +22,34 @@ def now():
     return datetime.datetime.now().strftime("%H:%M")
 
 
-class AlertWindow(Tkinter.Toplevel):
+class AlertWindow(tkinter.Toplevel):
     def __init__(self, message):
-        Tkinter.Toplevel.__init__(self)
+        tkinter.Toplevel.__init__(self)
 
         self.title(message)
 
-        self.message = Tkinter.StringVar()
+        self.message = tkinter.StringVar()
         self.message.set(message)
 
-        self.message_label = Tkinter.Label(self, textvariable=self.message, anchor='w', justify='left', padx=32, pady=32)
-        self.message_label.pack(fill='both', expand=1)
+        self.message_label = tkinter.Label(self, textvariable=self.message, anchor="w", justify="left", padx=32, pady=32)
+        self.message_label.pack(fill="both", expand=1)
 
 
-class MainWindow(Tkinter.Tk):
+class MainWindow(tkinter.Tk):
     def __init__(self):
-        Tkinter.Tk.__init__(self)
+        tkinter.Tk.__init__(self)
 
         self.after_id = None
         self.compare_against = ""
 
         self.title("Watching a site...")
 
-        self.header_label = Tkinter.Label(self, text=u"Watching this URL for changes every {1} seconds:\n{0}".format(SITE_URL, TIME_BETWEEN_CHECKS//1000), anchor='w', justify='left')
-        self.header_label.pack(fill='both', expand=1)
+        self.header_label = tkinter.Label(self, text="Watching this URL for changes every {1} seconds:\n{0}".format(SITE_URL, TIME_BETWEEN_CHECKS//1000), anchor="w", justify="left")
+        self.header_label.pack(fill="both", expand=1)
 
-        self.message = Tkinter.StringVar()
-        self.message_label = Tkinter.Label(self, textvariable=self.message, anchor='w', justify='left')
-        self.message_label.pack(fill='both', expand=1)
+        self.message = tkinter.StringVar()
+        self.message_label = tkinter.Label(self, textvariable=self.message, anchor="w", justify="left")
+        self.message_label.pack(fill="both", expand=1)
 
         # Global quit handlers
         self.bind_all("<Control-q>", self.quit_handler)
@@ -63,33 +62,33 @@ class MainWindow(Tkinter.Tk):
         self.after_id = self.after(TIME_BETWEEN_CHECKS, self.check_for_changes)
 
     def get_url(self):
-        return urllib2.urlopen(SITE_URL).read()
+        return urllib.request.urlopen(SITE_URL).read()
 
     def first_check(self):
         try:
-            self.message.set(u"[{0}] Getting the inital data...".format(now()))
+            self.message.set("[{0}] Getting the inital data...".format(now()))
             self.compare_against = self.get_url()
             self.start_timer()
-            self.message.set(u"[{0}] Waiting for the next check...".format(now()))
-        except urllib2.URLError as e:
-            self.message.set(u"[{0}] Error while getting the inital data:\n{1}\nThis program won't work, please restart it.".format(now(), e.reason))
+            self.message.set("[{0}] Waiting for the next check...".format(now()))
+        except urllib.error.URLError as e:
+            self.message.set("[{0}] Error while getting the inital data:\n{1}\nThis program won't work, please restart it.".format(now(), e.reason))
 
     def check_for_changes(self):
         try:
-            self.message.set(u"[{0}] Checking URL...".format(now()))
+            self.message.set("[{0}] Checking URL...".format(now()))
             new_data = self.get_url()
 
             if new_data == self.compare_against:
                 self.start_timer()
-                self.message.set(u"[{0}] No changes yet. Waiting for the next check...".format(now()))
+                self.message.set("[{0}] No changes yet. Waiting for the next check...".format(now()))
             else:
-                self.message.set(u"[{0}] The contents have been changed! \\o/".format(now()))
+                self.message.set("[{0}] The contents have changed! \\o/".format(now()))
                 self.message_label.config(fg="red")
-                self.alert_window = AlertWindow(u"[{0}] The contents have been changed! \\o/".format(now()))
+                self.alert_window = AlertWindow("[{0}] The contents have changed! \\o/".format(now()))
 
-        except urllib2.URLError as e:
+        except urllib.error.URLError as e:
             self.start_timer()
-            self.message.set(u"[{0}] Error while getting the URL:\n{1}\nTrying again later...".format(now(), e.reason))
+            self.message.set("[{0}] Error while getting the URL:\n{1}\nTrying again later...".format(now(), e.reason))
 
 
 def main():
